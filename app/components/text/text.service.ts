78ee36c16f8643f616d30eca8e77243f38a7636c
Jimmy Chen <jimmy@Jimmys-MBP.lan>
2017-04-30 22:46:54 -0600

    adding draft upload text

--- app/components/text/text.service.ts
+++ app/components/text/text.service.ts
@@ -7,11 +7,21 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class TextService{
      @Input() sourceUrl;
+     @Input() uploadUrl;
      private content:BasicComponentContent;
      constructor(private http:Http){
      }
     
      getContent():Promise<BasicComponentContent>{
-        return this.http.get(this.sourceUrl).toPromise().then(response => response.json() as BasicComponentContent)
-    }
+        return this.http.get(this.sourceUrl).toPromise().then(response => response.json() as BasicComponentContent);
+     }
+     
+     uploadText(componentPath:string[], text:string):Promise<BasicComponentContent>{
+        var form = new FormData();
+        form.append("component", componentPath);
+        form.append("text", text);
+        return this.http.post(this.uploadUrl, form).toPromise().then(
+            response => response.json() as BasicComponentContent
+        );
+     }
 }
